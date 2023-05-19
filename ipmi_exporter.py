7065ef7587a86b32af705493bad293d16a951016
Keith Valin <kvalin@redhat.com>
2023-05-19 18:05:33 -0400

    Initial commit, creates and parses CSV, just need to put in the header and transform it

---
+++ ipmi_exporter.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+import argparse
+import os
+import time
+import io
+import csv
+import subprocess
+
+TMP_FILE="/tmp/ipmi.csv"
+IPMI_COMMAND=f"ipmitool sensor | sed -e 's/[[:blank:]]*|[[:blank:]]*/,/g' | cut -d, -f1-3 > {TMP_FILE}"
+
+def main():
+    parser = argparse.ArgumentParser(
+        prog="ipmi_exporter",
+        description="Exports impi sensor data to a CSV"
+    )
+    _options(parser)
+    args = parser.parse_args()
+    run(args.file, args.time_frequency)
+
+def run(file: str, frequency: float):
+    create_header = not os.path.exists(file)
+
+    with open(file, 'a') as f:
+        try:
+            while True: #Do this infinitely
+                for row in get_ipmi_sensors():
+                    print(row)
+                time.sleep(frequency)
+        except KeyboardInterrupt:
+            print("Received Keyboard Interrupt, exiting")
+            return
+
+
+def get_ipmi_sensors():
+    proc = subprocess.run(IPMI_COMMAND, shell=True)
+    with open(TMP_FILE, 'r') as f:
+        return csv.reader(f.readlines())
+
+def _create_header(file: io.TextIOWrapper):
+    pass
+
+def _options(parser: argparse.ArgumentParser):
+    parser.add_argument(
+        "-t", "--time-frequency",
+        help="How long in seconds the polling interval is (default: 30, floats supported).",
+        action="store", required=False,
+        default=30, type=float
+    )
+    parser.add_argument(
+        "-f", "--file",
+        help="The destination file for the data from IPMI (default: ipmi.csv)",
+        action="store", default="ipmi.csv"
+    )
+
+
+if __name__ == "__main__":
+    main()
